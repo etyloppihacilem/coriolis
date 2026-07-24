411c6588b5c1640771bd52f6bb443693aeaaf7d9
Hippolyte Melica <hippolytemelica@gmail.com>
2026-07-24 11:34:56 +0200

    ODCGraph and ODCNode are now versatile

--- cumulus/src/plugins/odc/ODCGraph.py
+++ cumulus/src/plugins/odc/ODCGraph.py
@@ -11,25 +11,76 @@
 # |  Python      :   "./plugins/odc/ODCGraph.py"                    |
 # +-----------------------------------------------------------------+
 
-from .ODCNode import ODCNode
-from .Cuts import CutSetDB, Cut
+from coriolis.Hurricane import Instance, Net
+
+from .Cuts import Cut, CutSetDB
 from .InputsGrapher import InputsGrapher
+from .ODCNode import ODCNode
+
+
+class InputDict:
+    def __init__(self):
+        self.data = {}
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __getitem__(self, key):
+        try:
+            return self.data[key]
+        except KeyError:
+            new_dict = dict()
+            self.data[key] = new_dict
+            return new_dict
+
+    def __len__(self):
+        return sum([len(value) for value in self.data.values()])
+
+    def clean(self):
+        keys_to_remove = [key for value, key in self.data.items() if len(value) == 0]
+        for key in keys_to_remove:
+            del self.data[key]
 
 
 class ODCGraph:
-    def __init__(self, ff, info_cache):
+    """
+    Exactly one graph per flip-flop.
+    """
+
+    def __init__(self, ff: Instance, info_cache):
         self.node_db = {}
         self.info_cache = info_cache
-        self.top = ODCNode(self, ff, is_top=True)
+        self.top = ODCNode(self, ff, is_top=True) # this is the flip flop.
         self.cell_info = info_cache[ff]
         if not self.cell_info.isFlipflop:
             print("[ERROR] Can not build graph from non-flipflop cell.")
             raise ValueError
+        self.cut_db = CutSetDB()
+        self.global_inputs: dict[ODCNode, dict[str, Net]] = InputDict() # inputs of the whole graph.
+
+        # Parameters
         self.d_max = 20
         self.m = 11
         self.n_cap = 35
         self.max_inputs = 6
-        self.cut_db = CutSetDB()
+
+    def register_inputs(self, node):
+        for plug in node.instance.getPlugs():
+            master_net = plug.getMasterNet()
+            if (
+                master_net.getDirection() != Net.Direction.IN
+                or master_net.isSupply()
+                or master_net.isClock()
+            ):
+                continue
+            net = plug.getNet()
+            self.global_inputs[node][net.getName()] = net
+
+    def connect(self, node, net):
+        """
+        Used when an instance wants to connect to a nodes input.
+        """
+        self.global_inputs[node].pop(net.getName(), None)
 
     def add_node(self, instance):
         try:
@@ -40,6 +91,7 @@ class ODCGraph:
             return node
 
     def computeCuts(self):
+        self.top.makeChildren()
         self.getCuts(self.top)
         # OPTI:
         # self.cut_db.clear_except(self.top)
@@ -51,8 +103,8 @@ class ODCGraph:
             return
         for child in node.children:
             self.getCuts(child, level + 1)
-        combined = self.cut_db[node.children[0]]
-        for child in node.children[1:]:
+        combined = self.cut_db[node.children_list[0]]
+        for child in node.children_list[1:]:
             new_cuts = combined * self.cut_db[child]  # produit cartesien
             if new_cuts.maxSize() > self.m or len(new_cuts) > self.n_cap:
                 self.cut_db[node] = Cut(node)  # added in cut set automatically
@@ -78,3 +130,25 @@ class ODCGraph:
             if len(grapher.input_nets) > self.max_inputs:
                 discarded += 1
                 continue
+
+
+"""
+Donc il faut une manière unifiée d'exprimer les graphes (parce que pendant la construction, on peut se retrouver à
+parcourir à des étapes différentes la même node pour la parcourir pas du même sens et pas pour les mêmes raisons)
+Donc dans l'ordre :
+    - on fais le graphe depuis les cut et on enregistre toutes les inputs vides, puis on les strikes au fur et à mesure
+    - on parcours ces inputs et on construit le graph dans l'autre sens en back track en enregistrant et strikant aussi
+      les inputs que l'on crée sur le chemin
+    --- là on vérifie si on a bien 6 entrées ou moins
+    - on enregistre aussi les sorties primaires de tout le graphe normalement ce sont les cellules de cut...
+    - Il y a aussi un problème dans la façon dont est construit le graph de cut: si on coupe une porte et qu'un de ses enfants est aussi dans la cut, le lien entre les deux est supprimé.
+    - Donc il faut un graph global et rajouter dessus des vues qui sont des sortes de sous graphes qui se basent sur le graph de hurricane et l'agrandissent au besoin.
+    CHANGEMENT D'APPROCHE
+
+    On fait un seul graph qui représente tout hurricane et contient toutes les infos, notamment les fonctions exprimées par rapport aux nets, les enfants, les parents... Il faut un moyen de savoir si les parents et enfants ont étés explorés (certainement le tableau à none). On arrête d'utiliser une liste pour les parents et les enfants, maintenant c'est un dict qui fait [node, net], toutes les nodes sont du même type. on utilise que le nom des nets, tous les noms de pin devront être convertis. On affiche jamais la clock ni les alims. Le reset il faut vraiment y reflechir parce qu'on va pas clock gater le reset...
+    Par défaut, l'exploration des nodes s'arrête à une bascule ou au bout du circuit. Elle peut être vers les enfants ou les parents. Elle se propage. Le graphe à un dict [nom_d'instance, node] (pour que ce soit hashable). On peut dire que si une node est déjà explorée dans un sens, on a pas besoin de le refaire. Mais attentions aux infos qui pourraient manquer si l'exploration n'est pas complète. Chaque node est du même objet. On fait des objets qui contiennent des données sur les nodes à la limite, on essaye de ne pas stocker de données dans les nodes parce que c'est super chiant a extraire après. par exemple pour la récursion dans le parcours du graphe...
+
+    Pour les vues, c'est des surcharges des fonctions de parcours du graphe mais avec un paramètre de set qui contient des nodes. Pour tout parcours, on ajoute le set et si une node n'est pas dans le set, on considère qu'elle n'existe pas.
+
+    Peut être arrêter de faire de la récursion et mettre en place des piles, ce sera plus propre.
+"""
--- cumulus/src/plugins/odc/ODCNode.py
+++ cumulus/src/plugins/odc/ODCNode.py
@@ -12,49 +12,60 @@
 # +-----------------------------------------------------------------+
 
 from coriolis.Hurricane import Instance, Net
-from .HurricaneAsGraph import getChildren
+from .HurricaneAsGraph import getChildren, getParents, HurricaneHashasble
 
 
 class ODCNode:
     def __init__(self, graph, instance: Instance, is_top=False):
         self.instance = instance
-        self.info = graph.info_cache[instance]
-        self.children = []
-        self.children_net = {}
-        self.parents = []
-        self.parents_net = {}
-        for child, net in getChildren(self.instance):
-            if child is None:
-                continue
-            child_info = graph.info_cache[child]
-            if child_info.isFlipflop:
-                continue
-            new_node = graph.add_node(child)
-            new_node.markInput(self, net)
-            self.children.append(new_node)
-            self.children_net[new_node] = net
-        self.inputs: dict[str, Net] = {}  # clock or power supply are no input
+        self.graph = graph
+        self.children = {}
+        self.parents = {}
         # OPTI: is reset an input ?
         self.is_top = is_top
         if not is_top:
-            for plug in self.instance.getPlugs():
-                master_net = plug.getMasterNet()
-                if (
-                    master_net.getDirection() != Net.Direction.IN
-                    or master_net.isSupply()
-                    or master_net.isClock()
-                ):
-                    continue
-                net = plug.getNet()
-                self.inputs[net.getName()] = net
+            self.graph.register_inputs(self)
 
-    def markInput(self, parent, net):
-        self.inputs.pop(net.getName(), None)
-        self.parents.append(parent)
-        self.parents_net[parent] = net
+    def makeChildren(self):
+        for child, net in getChildren(self.instance):
+            if child is None:
+                continue
+            child_info = self.graph.info_cache[child]
+            if child_info.isFlipflop:
+                continue
+            new_node = self.graph.add_node(child)
+            new_node.addParent(self, net)
+            self.graph.connect(new_node, net)
+            self.children[new_node] = net
+
+    def makeParents(self):
+        for parent, net in getParents(self.instance):
+            if parent is None:
+                continue
+            parent_info = self.graph.info_cache[parent]
+            if parent_info.isFlipflop:
+                continue
+            new_node = self.graph.add_node(parent)
+            new_node.addChild(self, net)
+            self.graph.connect(self, net)
+            self.parents[new_node] = net
+
+    def addParent(self, parent, net):
+        self.parents[parent] = net
+
+    def addChild(self, child, net):
+        self.children[child] = net
 
     def __hash__(self):
         return hash(self.instance.getName())
 
     def getName(self):
         return self.instance.getName()
+
+    @property
+    def children_list(self):
+        return list(self.children.keys())
+
+    @property
+    def parent_list(self):
+        return list(self.children.keys())
